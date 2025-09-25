a35c2fa1ef8738a7532f58f1e34b5c4ecf964b96
bhagyeshagresar <bhagyeshagresar@gmail.com>
2025-09-24 20:03:47 -0500

    Added gitignore file

--- stm32f4xx_drivers/Debug/Src/main.d
+++
@@ -1,3 +0,0 @@
-Src/main.o: ../Src/main.c \
- C:/Users/bhagy/STM32CubeIDE/workspace_1.18.1/stm32f4xx_drivers/drivers/Inc/stm32f401xx.h
-C:/Users/bhagy/STM32CubeIDE/workspace_1.18.1/stm32f4xx_drivers/drivers/Inc/stm32f401xx.h:
